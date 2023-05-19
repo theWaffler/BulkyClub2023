cc3bf5a3ea6554e4a91cda2b16fb7035b51aa232
Gerald Judianto <gjudianto0@saddleback.edu>
2023-05-18 21:47:42 -0700

    makefiles

--- build-bulkClub-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-bulkClub-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -38,20 +38,48 @@ namespace {
 #ifdef QT_MOC_HAS_STRINGDATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
 static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
-    "MainWindow"
+    "MainWindow",
+    "on_pushButton_searchSalesReport_clicked",
+    "",
+    "on_pushButton_salesReportMemberTypeDisplay_clicked",
+    "on_pushButton_itemSold_clicked",
+    "on_pushButton_totalRevenueTax_clicked",
+    "on_pushButton_memberShoppingDataSearch_clicked",
+    "on_pushButton_memberType_clicked"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[2];
+    uint offsetsAndSizes[16];
     char stringdata0[11];
+    char stringdata1[40];
+    char stringdata2[1];
+    char stringdata3[51];
+    char stringdata4[31];
+    char stringdata5[38];
+    char stringdata6[47];
+    char stringdata7[33];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
     {
-        QT_MOC_LITERAL(0, 10)   // "MainWindow"
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 39),  // "on_pushButton_searchSalesRepo..."
+        QT_MOC_LITERAL(51, 0),  // ""
+        QT_MOC_LITERAL(52, 50),  // "on_pushButton_salesReportMemb..."
+        QT_MOC_LITERAL(103, 30),  // "on_pushButton_itemSold_clicked"
+        QT_MOC_LITERAL(134, 37),  // "on_pushButton_totalRevenueTax..."
+        QT_MOC_LITERAL(172, 46),  // "on_pushButton_memberShoppingD..."
+        QT_MOC_LITERAL(219, 32)   // "on_pushButton_memberType_clicked"
     },
-    "MainWindow"
+    "MainWindow",
+    "on_pushButton_searchSalesReport_clicked",
+    "",
+    "on_pushButton_salesReportMemberTypeDisplay_clicked",
+    "on_pushButton_itemSold_clicked",
+    "on_pushButton_totalRevenueTax_clicked",
+    "on_pushButton_memberShoppingDataSearch_clicked",
+    "on_pushButton_memberType_clicked"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -63,12 +91,28 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   50,    2, 0x08,    1 /* Private */,
+       3,    0,   51,    2, 0x08,    2 /* Private */,
+       4,    0,   52,    2, 0x08,    3 /* Private */,
+       5,    0,   53,    2, 0x08,    4 /* Private */,
+       6,    0,   54,    2, 0x08,    5 /* Private */,
+       7,    0,   55,    2, 0x08,    6 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -81,16 +125,38 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_pushButton_searchSalesReport_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_salesReportMemberTypeDisplay_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_itemSold_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_totalRevenueTax_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_memberShoppingDataSearch_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_memberType_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
 
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_pushButton_searchSalesReport_clicked(); break;
+        case 1: _t->on_pushButton_salesReportMemberTypeDisplay_clicked(); break;
+        case 2: _t->on_pushButton_itemSold_clicked(); break;
+        case 3: _t->on_pushButton_totalRevenueTax_clicked(); break;
+        case 4: _t->on_pushButton_memberShoppingDataSearch_clicked(); break;
+        case 5: _t->on_pushButton_memberType_clicked(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
@@ -110,6 +176,17 @@ void *MainWindow::qt_metacast(const char *_clname)
 int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 6;
+    }
     return _id;
 }
 QT_WARNING_POP
